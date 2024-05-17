9cde6bcdcbb14815d403c13bb3ede5dd40872eff
Mohibur Rahman <prmths43@gmail.com>
2024-05-17 20:28:16 +0600

    Update cluster_formation.py

--- Percolation/Python/cluster_formation.py
+++ Percolation/Python/cluster_formation.py
@@ -1,1 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 
+p = 0.5
+z = np.random.rand(100,100)
+m = z<p
+sns.heatmap(m, cmap='viridis')
+plt.show()
