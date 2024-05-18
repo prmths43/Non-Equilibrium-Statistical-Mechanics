e5601ce537cc3dd6684ece8ef0a92a548d32f98a
Mohibur Rahman <prmths43@gmail.com>
2024-05-18 16:05:33 +0600

    Update Numerical_measurement_of_the_cluster_number_density.py

--- Percolation/Python/Numerical_measurement_of_the_cluster_number_density.py
+++ Percolation/Python/Numerical_measurement_of_the_cluster_number_density.py
@@ -1,1 +1,15 @@
+from pylab import *
+from scipy.ndimage import label
+from scipy.ndimage import sum
 
+L = 20
+p = 0.90
+z = rand(L)
+m = z<p
+lw, num = label(m)
+
+labelList = arange(lw.max() + 1)
+area = sum(m, lw, labelList)
+
+print(*labelList, sep = ',')
+print(*area, sep = ',')
